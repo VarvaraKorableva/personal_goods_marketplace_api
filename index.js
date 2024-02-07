fc09f9875bf3f1899afe871b8152521de606494a
Varvara Korableva <v.korableva1990@yandex.ru>
2024-02-07 23:49:28 +0200

    added category change like pic and added slug

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ import { item_router } from "./routes/items.routes.js"
 import { messages_router } from "./routes/messages.routes.js"
 import {favoriteItems_router} from "./routes/favoriteItems.routes.js"
 import {reviews_router} from "./routes/reviews.routes.js"
-
 //import { files_router } from "./routes/upload.routes.js";
+
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors());
+
+app.use(cors({
+  credentials: true,
+  origin: [
+    'http://localhost:3001',
+    'http://localhost:3000',
+    'https://localhost:3001',
+    'https://localhost:3000',
+  ],
+}));
 
 app.use("/users", users_router);
 app.use("/category", category_router);
@@ -23,8 +32,7 @@ app.use("/items", item_router);
 app.use("/messages", messages_router);
 app.use("/favoriteItems", favoriteItems_router);
 app.use("/reviews", reviews_router);
-
-//app.use("/", files_router);
+//app.use("/files", files_router); //images
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`run on ${process.env.PORT || 8080}`);
--- models/category.models.js
+++ models/category.models.js
@@ -25,7 +25,34 @@ export const _getAllCategory = async () => {
   
 export const _deleteCategory = (category_id) => {
     return db("category").delete("*").where({ category_id })
-  };
+};
+
+export const _updateCategory = async (category_id, image_url) => {
+  console.log({image_url})
+  try {
+      const result = await db("category")
+          .where({ category_id })
+          .update({image_url})
+          .returning("*");
+
+      return result[0];
+  } catch (error) {
+      throw new Error(`Error updating category: ${error.message}`);
+  }
+};
+
+export const _updateCategorySlug = async (category_id, slug) => {
+  try {
+      const result = await db("category")
+          .where({ category_id })
+          .update({slug})
+          .returning("*");
+
+      return result[0];
+  } catch (error) {
+      throw new Error(`Error updating category: ${error.message}`);
+  }
+};
 
 /*
 CREATE TABLE category (
@@ -35,4 +62,10 @@ CREATE TABLE category (
     parent_id INTEGER,//если я не передаю знаючит это категория, если передаю, это субкатегория
     FOREIGN KEY (parent_id) REFERENCES category(category_id) ON DELETE CASCADE
 );
+
+ALTER TABLE category
+ADD COLUMN image_url VARCHAR(255) DEFAULT 'https://images.unsplash.com/photo-1618278559289-d2070c065a17?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8ODN8fGNhdCUyMG9uJTIwZ3JleSUyMGJhY2tncmF1bmR8ZW58MHx8MHx8fDA%3D';
+
+ALTER TABLE category
+ADD COLUMN slug VARCHAR(30);
  */
