4d1eee74ca8bf1e2416059986a86bc3e3facfc96
Varvara Korableva <v.korableva1990@yandex.ru>
2024-02-08 10:46:07 +0200

    changed reg, added cookies, deleted phone from models and controllers

--- models/users.models.js
+++ models/users.models.js
@@ -1,15 +1,14 @@
 import { db } from "../config/pg.config.js"
 import bcrypt from 'bcrypt'
 
-export const _createUser = async (username, email, password, phone) => {
+export const _createUser = async (username, email, password) => {
     try {
       const hashedPassword = await bcrypt.hash(password, 10);
   
       const result = await db("users").insert({
         username,
         email,
-        password: hashedPassword,
-        phone
+        password: hashedPassword
       }).returning("*");
   
       return result[0];
@@ -33,3 +32,23 @@ export const _getAllUsers = () => {
 export const _deleteUser = (user_id) => {
   return db("users").delete("*").where({ user_id })
 };
+
+
+/*
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL
+);
+
+ALTER TABLE users
+ADD CONSTRAINT unique_email UNIQUE (email);
+
+ALTER TABLE users
+ADD COLUMN created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL;
+
+ALTER TABLE users
+ALTER COLUMN phone DROP NOT NULL;
+*/
