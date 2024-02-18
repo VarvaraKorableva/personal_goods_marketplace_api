0e0daf42ea135eb466bde293520f4d48533ef935
Varvara Korableva <v.korableva1990@yandex.ru>
2024-02-19 00:06:59 +0200

    added aws

--- controllers/upload.controllers.js
+++ controllers/upload.controllers.js
@@ -1,13 +1,49 @@
-import { uploadSingle } from "../models/upload.models.js";
-
+import { uploadSingle, _getAllImages, _getAllImagesByUserId, _deleteImage } from "../models/upload.models.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 export const _uploadSingle = async (req, res) => {
+
   const { key, mimetype, location, originalname } = req.file;
-  const { item_id } = req.item_id;
+  const item_id = Number(req.body.str_item_id)
+
   try {
-    const row = await uploadSingle({ key, mimetype, location, originalname, item_id });
-    res.json(row);
+    const data = await uploadSingle(item_id, key, mimetype, location, originalname);
+    console.log(' =>', data)
+    res.json(data)
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const getAllImagesByUserId = (req, res) => {
+  const { owner_id } = req.params
+  _getAllImagesByUserId(owner_id)
+  .then((data) => {
+      res.json(data);
+  })
+  .catch((err) => {
+      res.status(404).json({ msg: "Not Found" });
+  });
+};
+
+export const deleteImage = (req, res) => {
+  const { images_of_goods_id } = req.params
+  _deleteImage(images_of_goods_id)
+  .then((data) => {
+      res.json({ msg: "Deleted" })
+  })
+  .catch((err) => {
+      res.status(404).json({ msg: "Not Found" });
+  });
+};
+
+export const getAllImages = (req, res) => {
+  _getAllImages()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.status(404).json({ msg: "Not Found" });
+      });
+}; 
--- models/upload.models.js
+++ models/upload.models.js
@@ -1,27 +1,50 @@
 import { db } from "../config/pg.config.js";
 /*
-export const uploadSingle = ({ key, mimetype, location, originalname, item_id }) => {
-  return db("uploads").insert({ key, mimetype, location, originalname, item_id }, [
+export const uploadSingle = async (item_id, key, mimetype, location, originalname ) => {
+  return db("uploads").insert({ item_id, key, mimetype, location, originalname }, [
+    "item_id",
     "key",
     "mimetype",
     "location",
     "originalname",
-    "item_id",
   ]);
-};*/
+}; */
 
-export const uploadSingle = async (key, mimetype, location, originalname, item_id) => {
+export const uploadSingle = async (item_id, key, mimetype, location, originalname) => {
+
   try {
-    const result = await db("uploads").insert({
-      key, mimetype, location, originalname, item_id
-    }).returning("*");
-
+    const result = await db("uploads")
+    .insert({ item_id, key, mimetype, location, originalname })
+    .returning("*");
     return result[0];
-  }catch (error) {
-    throw new Error(`Error creating image: ${error.message}`);
+  } catch (error) {
+    console.error("Ошибка при вставке данных:", error);
+    return null;
   }
 };
 
+
+export const _getAllImagesByUserId = async (owner_id) => {
+  try {
+      const result = await db("uploads").select("*").where({ owner_id })
+      return result
+  } catch (error) {
+      throw new Error(`error: ${error.message}`);
+  }
+};
+
+export const _getAllImages = async () => {
+  try {
+      const result = await db("uploads").select("*")
+      return result
+  } catch (error) {
+      throw new Error(`Error in _getAllItemsImagesByUserId: ${error.message}`);
+  }
+};
+
+export const _deleteImage = (images_of_goods_id) => {
+  return db("uploads").delete("*").where({ images_of_goods_id })
+};
 
 
 /*
@@ -30,7 +53,7 @@ create table uploads(
  item_id INTEGER,
  key varchar(255),
  mimetype varchar(50),
- location varchar(1000),
+ location varchar(1000),////
  originalname varchar(1000),
 
  FOREIGN KEY (item_id) REFERENCES items(item_id) ON DELETE CASCADE
