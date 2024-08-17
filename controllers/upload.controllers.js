480851d6f4fd5d08853eb5e3cdf88f51b24f4ea4
Varvara Korableva <v.korableva1990@yandex.ru>
2024-08-17 18:03:24 +0300

    added few fotos

--- controllers/upload.controllers.js
+++ controllers/upload.controllers.js
@@ -1,4 +1,4 @@
-import { uploadSingle, _getAllImages, _getAllImagesByUserId, _deleteImage } from "../models/upload.models.js";
+import { _getAllImagesByItemId, uploadSingle, _getAllImages, _getAllImagesByUserId, _deleteImage, _uploadMultiple } from "../models/upload.models.js";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -14,9 +14,21 @@ export const _uploadSingle = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+//_getAllImagesByItemId
+export const getAllImagesByItemId = (req, res) => {
+  const { item_id } = req.params
+  _getAllImagesByItemId(item_id)
+  .then((data) => {
+      res.json(data);
+  })
+  .catch((err) => {
+      res.status(404).json({ msg: "Not Found" });
+  });
+};
 
 export const getAllImagesByUserId = (req, res) => {
   const { owner_id } = req.params
+  
   _getAllImagesByUserId(owner_id)
   .then((data) => {
       res.json(data);
@@ -46,3 +58,23 @@ export const getAllImages = (req, res) => {
         res.status(404).json({ msg: "Not Found" });
       });
 }; 
+
+// Обработка нескольких файлов
+export const uploadMultiple = async (req, res) => {
+  const { user_id, str_item_id } = req.body;
+  
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).json({ error: "No files uploaded" });
+  }
+
+  try {
+    const uploadedFiles = await _uploadMultiple(user_id, str_item_id, req.files);
+    res.json({
+      message: `${Object.keys(req.files).length} files uploaded successfully`,
+      files: uploadedFiles,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
