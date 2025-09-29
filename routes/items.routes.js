1db3457100d14ebf73181a7dd7a521638f623da4
Varvara Korableva <v.korableva1990@yandex.ru>
2025-09-29 14:23:23 +0300

    deleted itemsfilter models and controllers

--- routes/items.routes.js
+++ routes/items.routes.js
@@ -10,18 +10,14 @@ import {
     updatePrice,
     updateDescription,
     updateCondition,
-    //getItemsByCategoryRecursive,
     getItemsController,
 } from "../controllers/items.controllers.js";
-import { getItemsByFilter } from "../controllers/itemsSearch.controllers.js"
 
 item_router.get('/userId/:owner_id', getAllItemsByUserId)
 item_router.get('/all/getItems', getItemsController)
 item_router.get('/all/:item_id', getItemById)
-item_router.get('/getItemsByFilter', getItemsByFilter)
 item_router.post('/', createItem)
 item_router.patch('/:item_id', deleteItem)
-//item_router.get('/getItemsByCategoryRecursive/:category_id', getItemsByCategoryRecursive) //////
 item_router.patch('/updateIsReserved/:item_id/:user_id', updateIsReserved)
 item_router.patch('/updateCity/:item_id', updateCity)
 item_router.patch('/updatePrice/:item_id', updatePrice)
