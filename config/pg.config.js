25ca4ef666b1aed30434848f599acf73da371317
Varvara Korableva <v.korableva1990@yandex.ru>
2025-09-29 17:59:40 +0300

    deleted second db

--- config/pg.config.js
+++ config/pg.config.js
@@ -1,23 +1,14 @@
+// db.js
 import knex from "knex";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-// Определяем, что используется: продакшен или локальная разработка
-const isProduction = process.env.NODE_ENV === "production";
-
-// Public URL для локальной разработки
-// Internal URL для продакшена (Render)
-const connectionString = isProduction
-  ? process.env.DATABASE_INTERNAL_URL
-  : process.env.DATABASE_URL;
-
 export const db = knex({
   client: "pg",
   connection: {
-    connectionString,
-    // SSL включаем только для Public URL
-    ssl: isProduction ? undefined : { rejectUnauthorized: false }
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
   },
   migrations: {
     directory: "./migrations"
